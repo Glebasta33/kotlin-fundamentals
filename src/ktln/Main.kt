62aceb08b00a1eaeaeebd2f11b36b622dc549717
Glebasta33 <gleb.trusow@yandex.ru>
2021-09-30 09:15:29 +0300

    nullable variables

--- src/ktln/Main.kt
+++ src/ktln/Main.kt
@@ -1,5 +1,9 @@
 package ktln
 
 fun main() {
-    println("Hello Kotlin!")
+    val name: String? = "Joe"
+    val last: String? = "Johna"
+    val book: String? = null
+    val sum = (name?.length?:0) + (last?.length?:0) + (book?.length?:0)
+    println(sum)
 }
