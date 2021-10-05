371ef06a20b8f4d047e32dcb5ab1c1fda68ce337
Glebasta33 <gleb.trusow@yandex.ru>
2021-10-05 11:45:57 +0300

    bubble sorting

--- src/jv/Main.java
+++ src/jv/Main.java
@@ -2,6 +2,32 @@ package jv;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String month = "Октябрь";
+        String season;
+        switch (month) {
+            case "Декабрь":
+            case "Январь":
+            case "Февраль":
+                season = "Зима";
+                break;
+            case "Март":
+            case "Апрель":
+            case "Май":
+                season = "Весна";
+                break;
+            case "Июнь":
+            case "Июль":
+            case "Август":
+                season = "Лето";
+                break;
+            case "Сентябрь":
+            case "Октябрь":
+            case "Ноябрь":
+                season = "Осень";
+                break;
+            default:
+                season = "Такого месяца нет";
+        }
+        System.out.println(season);
     }
 }
--- src/ktln/Main.kt
+++ src/ktln/Main.kt
@@ -1,9 +1,26 @@
 package ktln
 
 fun main() {
-    val name: String? = "Joe"
-    val last: String? = "Johna"
-    val book: String? = null
-    val sum = (name?.length?:0) + (last?.length?:0) + (book?.length?:0)
-    println(sum)
+    var list: MutableList<Int> = mutableListOf(23, 43, 1, 5, 34, 4, 9, 100, 50, 250)
+    println(sort(123, 4, 0, 12))
 }
+
+fun sort(numbers: MutableList<Int>) : List<Int> {
+    for (i in 0 until numbers.size) {
+        for (j in numbers.size - 1 downTo i) {
+            var x: Int
+            if (numbers[i] > numbers[j]) {
+                x = numbers[j]
+                numbers[j] = numbers[i]
+                numbers[i] = x
+            }
+        }
+    }
+    return numbers
+}
+
+fun sort(array: Array<Int>) = sort(array.toMutableList())
+fun sort(vararg numbers: Int) = sort(numbers.toMutableList())
+
+
+
